a1bc7ec1be68611ef9096cb36603a1ee763be42e
golichakki-cyber <golichakki@gmail.com>
2026-03-22 17:37:26 +0530

    Cleanup unneeded files (logs, test scripts, and temporary XML artifacts)

--- test_server.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('OK'));
-app.listen(3001, () => console.log('Minimal server running on 3001'));
-setTimeout(() => {}, 10000); // Keep alive for 10s
